6aed262c6fb3925462d947e9432e45546b91bf51
Ben Liebeskind <bliebeskind@austin.utexas.edu>
2016-06-14 13:24:14 -0500

    Modifications for generalizability of PhysPopulation
    Added a parameter dictionary self.paramD mapping string names to param field
    values. Also changed self.binding_curve to generic self.output

--- evophys/models/binding/ssb.py
+++ evophys/models/binding/ssb.py
@@ -9,32 +9,32 @@ import numpy as np
 class SSB:
 
     def __init__(self,k1=1e7,k2=1e8,f=10):
-        self.k1 = k1
-        self.k2 = k2
-        self.f = f
-        self.w = None
+		self.k1 = k1
+		self.k2 = k2
+		self.f = f
+		self.w = None
 		
 		self.paramD = {"k1":self.k1,"k2":self.k2,"f":self.f}
 		
-        self.conc_range = (-12,-3)
+		self.conc_range = (-12,-3)
 		self.xvals = []
 		
-        self.binding_curve = []
-        
-        self.step = .01
-        self.step_func = lambda x: 10**x
+		self.output = []
+		
+		self.step = .01
+		self.step_func = lambda x: 10**x
 
-        self.function = lambda x: (k1*x + k2*x + f*2*k1*k2*x**2) / (2 * (1 + k1*x + k2*x + f*k1*k2*x**2))
-        
-        self.sim()
+		self.function = lambda x: (k1*x + k2*x + f*2*k1*k2*x**2) / (2 * (1 + k1*x + k2*x + f*k1*k2*x**2))
+		
+		self.sim()
 
     def sim(self):
-        self.binding_curve = []
+        self.output = []
         self.xvals = []
         for val in np.arange(self.conc_range[0],self.conc_range[1],self.step):
             if self.step_func:
                 self.xvals.append(self.step_func(val))
-                self.binding_curve.append(self.function(self.step_func(val)))
+                self.output.append(self.function(self.step_func(val)))
             else:
                 self.xvals.append(val)
-                self.binding_curve.append(self.function(val))
+                self.output.append(self.function(val))
