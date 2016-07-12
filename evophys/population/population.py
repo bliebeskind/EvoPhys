64752261d0fa9769a33d0124ad13d723ed721692
Ben Liebeskind <bliebeskind@austin.utexas.edu>
2016-07-12 17:06:26 -0500

    Changed PhysPopulation to fit the new mutation model.
    Mutations are now separate from population models. Population models should now
    call the mutation classes .mutate() method, which takes in and spits out the
    parameter dictionary for the model.
    
    I have not edited Wright-Fisher for this

--- evophys/population/population.py
+++ evophys/population/population.py
@@ -14,21 +14,25 @@ class PhysPopulation():
 	A population of user-specified biophysical models
 	'''
 
-	def __init__(self,model,modelParams={},params_to_mut=[],N=100,dt=10,selection_strength=1,mutAllParams=True,startRandom=False,paramBounds=None):
+	def __init__(self,startModel,mutModel,N=100,selection_strength=1,startRandom=False,paramBounds=None):
 		
 		# Biophysical model
 		# Create target model on startup by simulating under binding model and populating self.binding curve
-		self.model = model
-		self._model_params = modelParams
-		self._model_inst = self.model(**modelParams)
-		self.target = self._model_inst.output
-		
-		self.params_to_mut = params_to_mut # params in model to explore. Must specify so some can be left static
-		self._validate_params()
-		self.param_means = {i:None for i in self._model_inst.paramD}
-		self.param_vars = {i:None for i in self._model_inst.paramD}
+		self.start_model = startModel
+		self.model = self.start_model.__class__
+		self._model_params = self.start_model.paramD
 
+		self.target = self.start_model.output
+
+		self.param_means = {i:None for i in self.start_model.paramD}
+		self.param_vars = {i:None for i in self.start_model.paramD}
 		
+		# Mutation model
+		self.mutator = mutModel
+		
+		#### Need a good validation here ####
+		self._validate_params()
+
 		# Population parameters
 		self.population = []
 		self.mean_fitness = 0
@@ -37,10 +41,6 @@ class PhysPopulation():
 		# Genetic algorithm params
 		self.N = N
 		self.selection_strength = selection_strength
-		self.mutAllParams = mutAllParams
-		
-		# Brownian motion params
-		self.dt = dt
 		
 		# Initialization
 		self.param_bounds = paramBounds
@@ -48,19 +48,21 @@ class PhysPopulation():
 		
 		
 	def _validate_params(self):
-		for i in self.params_to_mut:
-			assert i in self._model_inst.paramD, "Parameter %s not found in supplied model" % i
+		pass
 		
 	def _update_biophys_params(self):
 		'''Update means and variances for population K1 and K2s'''
-		paramVals = {p:[] for p in self._model_inst.paramD}
+		paramVals = {p:[] for p in self.start_model.paramD}
 		ws = []
 		for i in self.population:
-			for param in self._model_inst.paramD.keys():
-				paramVals[param].append(i.paramD[param])
+			for param,value in i.paramD.iteritems():
+				if param in paramVals:
+					paramVals[param].append(value)
+				else:
+					paramVals[param] = [value]
 			ws.append(i.w)
 			
-		for p in self._model_inst.paramD:
+		for p in paramVals:
 			self.param_means[p] = sum(paramVals[p])/len(paramVals[p])
 			self.param_vars[p] = np.var(paramVals[p])
 			
@@ -71,28 +73,31 @@ class PhysPopulation():
 		'''Create starting population of size self.N filled with Adair models, each sampled'''
 		
 		self.population = []
-		paramVals = {p:[] for p in self._model_inst.paramD}
+		paramVals = {p:[] for p in self.start_model.paramD}
 		ws = []
 		
 		for i in range(self.N):
 			if startRandom:
 				rando = np.random.uniform
-				for param in self._model_inst.paramD:
+				for param in self.start_model.paramD:
 					assert self.param_bounds and param in self.param_bounds, "Must supply bounds for parameter random start"
-				kwargs = {p: np.random.uniform(self.param_bounds[p][0],self.param_bounds[p][1]) for p in self.param_bounds}
-				newModel = self.model(**kwargs)
+				randParamD = {p: np.random.uniform(self.param_bounds[p][0],self.param_bounds[p][1]) for p in self.param_bounds}
+				newModel = self.model(paramD=randParamD)
 			else:
-				newModel = self.model(**self._model_params)
+				newModel = self.model(self._model_params)
 			
 			newModel.w = self._get_w(self.target,newModel.output)
 			
-			for param in self._model_inst.paramD:
-				paramVals[param].append(newModel.paramD[param])
+			for param,value in newModel.paramD.iteritems():
+				if param in paramVals:
+					paramVals[param].append(value)
+				else:
+					paramVals[param] = [value]
 			ws.append(newModel.w)
 			
 			self.population.append(newModel)
 			
-		for p in self._model_inst.paramD:
+		for p in paramVals:
 			self.param_means[p] = sum(paramVals[p])/len(paramVals[p])
 			self.param_vars[p] = np.var(paramVals[p])
 
@@ -107,46 +112,30 @@ class PhysPopulation():
 		assert len(comp) == len(target), "Different vector lengths: target: %i, comp: %i" % (len(target),len(comp))
 		rmsd = np.sqrt(sum(map(lambda (x,y): (x-y)**2, zip(target,comp)))/len(comp))
 		return np.exp(-(rmsd**2)/2*self.selection_strength)
-
-	def brownian(self,paramD):
-		'''Take a single brownian step over params vector'''
-		out = {}
-		if not self.mutAllParams:
-			paramToMut = np.random.choice(self.params_to_mut)
-		for i,j in paramD.iteritems():
-			if i not in self.params_to_mut:
-				out[i] = j # set to old value
-				continue
-			elif not self.mutAllParams and i != paramToMut:
-				out[i] = j # set to old value
-				continue
-			else:
-				new_param = j + norm.rvs(loc=0,scale=2*self.dt)
-				if new_param < 0:
-					new_param = 0
-				out[i] = new_param
-		return out
 		
 	def procreate(self):
 		newpop = []
-		paramVals = {p:[] for p in self._model_inst.paramD}
+		paramVals = {p:[] for p in self.start_model.paramD}
 		ws = []
 		
 		for model in self.population:
-			kwargs = self.brownian(model.paramD)
-			newModel = self.model(**kwargs)
+			mutParams = self.mutator.mutate(model.paramD)
+			newModel = self.model(mutParams)
 			
 			newModel.w = self._get_w(self.target,newModel.output)
 			
-			for param in self._model_inst.paramD:
-				paramVals[param].append(newModel.paramD[param])
+			for param,value in newModel.paramD.iteritems():
+				if param in paramVals:
+					paramVals[param].append(value)
+				else:
+					paramVals[param] = [value]
 			ws.append(newModel.w)
 			
 			newpop.append(newModel)
 			
 		self.population = newpop
 			
-		for p in self._model_inst.paramD:
+		for p in paramVals:
 			self.param_means[p] = sum(paramVals[p])/len(paramVals[p])
 			self.param_vars[p] = np.var(paramVals[p])
 			
@@ -157,6 +146,7 @@ class PhysPopulation():
 		fitness_vec = [m.w for m in self.population]
 		w_sum = sum(fitness_vec)
 		w_norm = [i/w_sum for i in fitness_vec]
+		self.fitness_vec = w_norm
 		new_pop = np.random.choice(self.population,self.N,p=w_norm)
 		self.population = new_pop
 		self._update_biophys_params()
