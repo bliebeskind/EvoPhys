0810628b744a439343b0fac0e829f878723efafe
Ben Liebeskind <bliebeskind@austin.utexas.edu>
2016-07-12 17:04:43 -0500

    Modified SSB for the new mutation framework and to allow dynamic site duplications
    SSB now takes a parameter dictionary as input. It works with the new mutation
    model SSBmut, which can do tandem duplications, and dynamically changes the
    equation to match the relevant number of sites

--- evophys/models/binding/ssb.py
+++ evophys/models/binding/ssb.py
@@ -1,20 +1,24 @@
 '''
-Single Site Binding model
+Site-specific Binding model
 other documentation....
 '''
 
+import random
 import numpy as np
+from itertools import combinations
 
 
-class SSB:
+class SSBdynam:
 
-    def __init__(self,k1=1e7,k2=1e8,f=10):
-		self.k1 = k1
-		self.k2 = k2
-		self.f = f
-		self.w = None
+	def __init__(self,paramD={"nk":2,"k1":1e7,"k2":1e8,"f12":10}):
 		
-		self.paramD = {"k1":self.k1,"k2":self.k2,"f":self.f}
+		assert type(paramD) is dict, "provide parameter dictionary"
+		self.paramD = paramD
+		assert self.paramD["nk"] > 0, "nk must be greater than 0"
+		self._construct_attributes()
+		self._construct_function()
+
+		self.w = None
 		
 		self.conc_range = (-12,-3)
 		self.xvals = []
@@ -23,18 +27,52 @@ class SSB:
 		
 		self.step = .01
 		self.step_func = lambda x: 10**x
-
-		self.function = lambda x: (k1*x + k2*x + f*2*k1*k2*x**2) / (2 * (1 + k1*x + k2*x + f*k1*k2*x**2))
 		
 		self.sim()
+		
+	def _construct_f(self,i,tup):
+		return "".join(["{f"+"".join(map(str,tup[j-2:]))+"}*" for j in range(i,2,-1)])
+		
+	def _construct_attributes(self):
+		for i,j in self.paramD.iteritems():
+			if i == "nk": j = int(j)
+			setattr(self,i,j)
+		
+	def _construct_function(self):
+		'''Construct the equilibrium binding equation'''
+		kpart = "+".join(["{k"+str(i)+"}*x" for i in range(1,self.nk+1)])
+		numerator = "("+kpart
+		denominator = "("+str(self.nk)+"*"+"(1+"+kpart
+		for i in range(2,self.nk+1):
+			for tup in combinations(range(1,self.nk+1),i):
+				numerator += "+"
+				denominator += "+"
+				numerator += "{}*".format(i) + "%s" % (self._construct_f(i,tup) if i > 2 else '') + "{f" + "".join(map(str,tup)) \
+								+"}*" + "*".join(map(lambda x: "{k"+str(x)+"}",tup)) + "*x**%d" % i
+				denominator += "%s" % (self._construct_f(i,tup) if i > 2 else '') + "{f" + "".join(map(str,tup)) +"}*" \
+								+ "*".join(map(lambda x: "{k"+str(x)+"}",tup)) + "*x**{}".format(i)
+		numerator += ")"
+		denominator += "))"
+		tmp = "lambda x: " + numerator + " / " + denominator
+		self.function_string = tmp.replace("}",'').replace("{",'').split(":")[1].strip()
+		self.function = eval(tmp.format(**self.paramD))
 
-    def sim(self):
-        self.output = []
-        self.xvals = []
-        for val in np.arange(self.conc_range[0],self.conc_range[1],self.step):
-            if self.step_func:
-                self.xvals.append(self.step_func(val))
-                self.output.append(self.function(self.step_func(val)))
-            else:
-                self.xvals.append(val)
-                self.output.append(self.function(val))
+	def sim(self):
+		'''Populate self.output with output from self.function'''
+		self.output = []
+		self.xvals = []
+		for val in np.arange(self.conc_range[0],self.conc_range[1],self.step):
+			if self.step_func:
+				self.xvals.append(self.step_func(val))
+				self.output.append(self.function(self.step_func(val)))
+			else:
+				self.xvals.append(val)
+				self.output.append(self.function(val))
+		
+	
+	
+	
+	
+	
+	
+				
