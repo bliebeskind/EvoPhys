d72f8286b69ab2f981c0f6b03165cf3c1bff40dc
Keegan <keegan.hines@gmail.com>
2016-08-15 22:49:44 -0400

    runtime bugs

--- evophys/mcmc/model_inference.py
+++ evophys/mcmc/model_inference.py
@@ -1,9 +1,9 @@
 import emcee
+import numpy as np
 from evophys.models.binding.binding_model import BindingModel
 
 class ModelInference(object):
 	def __init__(self, input_model):
-		raise NotImplementedError
 		assert(isinstance(input_model, BindingModel))
 		self.input_model = input_model
 		self.input_model_param_names = self.input_model.paramD.keys()
@@ -28,7 +28,7 @@ class ModelInference(object):
 		assert(len(theta) == len(self.input_model_param_names))
 		d = {}
 		for index, param_key in enumerate(self.input_model_param_names):
-			d[param_key] = theta[i]
+			d[param_key] = theta[index]
 
 		# there's going to be a LOT of object creation if we do it
 		# this way, but good enough start
@@ -41,16 +41,16 @@ class ModelInference(object):
 		
 	def __ln_posterior(self, theta, x, y):
 		lp = self.__ln_prior(theta)
-	    if not np.isfinite(lp):
-	        return -np.inf
-	    return lp + self._ln_likelihood(theta, x, y)
+		if not np.isfinite(lp):
+			return -np.inf
+		return lp + self.__ln_likelihood(theta, x, y)
 
-	def sample(self, iterations=1000, n_walkers=100, burnin=100):
+	def sample(self, iterations=1000, nwalkers=10, burnin=100):
 		# Set up the sampler.
 		ndim = len(self.input_model_param_names)
 		pos = [ 1e-4*np.random.randn(ndim) for i in range(nwalkers)]
 		self.sampler = emcee.EnsembleSampler(nwalkers, ndim, self.__ln_posterior, args=(self.x_data, self.y_data))
-		res = sampler.run_mcmc(pos, iterations, rstate0=np.random.get_state())
+		res = self.sampler.run_mcmc(pos, iterations, rstate0=np.random.get_state())
 
 		samples = self.sampler.chain[:, burnin:, :].reshape((-1, ndim))
 
